b02360db3a977cc4f8f67268ddf1773111bdaf80
Aaron Nader <65513748+AaronNader96@users.noreply.github.com>
2023-03-12 23:29:21 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = process.env.PORT || 80;
+
+app.set('view engine', 'ejs');
+
+// Load Static Assets
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/assets', express.static(path.join(path.join(__dirname, 'public/assets'))))
+
+// Home Route
+
+app.get('/',(req,res) =>{
+    res.render('base', {title: "Login System"});
+})
+
+
+app.listen(port, ()=>{ console.log("Listening to the server on port 3000.")});
+
+
